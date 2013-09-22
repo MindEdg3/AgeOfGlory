ffce594e3e987f93905578ae32d5fd9cd4de7a3f
mrimsh <murkorp@gmail.com>
2013-09-23 02:37:01 +0400

    Fix: Wall type saving in scene 2

--- Assets/Scripts/Editor/DungeonWallInspector.cs
+++ Assets/Scripts/Editor/DungeonWallInspector.cs
@@ -16,12 +16,25 @@ public class DungeonWallInspector : Editor
 			return _myTarget;	
 		}
 	}
+	
+	public int SelectedWall {
+		get {
+			return MyTarget.SelectedWall;
+		}
+		set {
+			if (MyTarget.SelectedWall != value) {
+				MyTarget.SelectedWall = value;
+				EditorUtility.SetDirty (MyTarget);
+			}
+		}
+	}
 
 	public override void OnInspectorGUI ()
 	{
 		base.OnInspectorGUI ();
 		
 		string[] walls = DungeonManager.Instance.WallTypes;
-		MyTarget.SelectedWall = EditorGUILayout.Popup ("Selected Wall", MyTarget.SelectedWall, walls);
+		SelectedWall = EditorGUILayout.Popup ("Selected Wall", SelectedWall, walls);
+		
 	}
 }
